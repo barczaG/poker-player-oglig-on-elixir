9010dd7bcbe498a2e0593947b912d467b61f5937
barczaG <gergo@barcza.hu>
2018-01-27 11:05:10 +0100

    Initial commit

---
+++ lib/hello_web/controllers/player_controller.ex
@@ -0,0 +1,29 @@
+defmodule HelloWeb.PlayerController do
+  use HelloWeb, :controller
+  @version "0.0.1"
+
+  def index(conn, params) do
+    IO.inspect params
+    IO.inspect params["action"]
+
+    action = params["action"]
+
+    case action do
+      "version" -> text conn, HelloWeb.PlayerController.version()
+      "bet_request" -> text conn, HelloWeb.PlayerController.bet_request()
+      "showdown" -> text conn, HelloWeb.PlayerController.showdown()
+      _ -> ""
+    end
+    # text conn, "Showing"
+  end
+
+  def bet_request() do
+    0
+  end
+
+  def showdown() do
+    "ok"
+  end
+
+  def version, do: @version
+end
