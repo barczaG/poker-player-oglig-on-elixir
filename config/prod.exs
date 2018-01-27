8165f3768a7663cf90bd1a1febe69dc96ecba3e1
barczaG <gergo@barcza.hu>
2018-01-27 12:12:23 +0100

    config change

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :hello, HelloWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  url: [ port: 80]
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]]
   # cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
